d5ef6aa8ae961ba00da13ba4de640979d11b818a
drumath <2237k.drumath@gmail.com>
2021-08-19 23:01:50 +0900

    :bug: immersal returns error

--- Assets/ImmersalRESTLocalizerTest/Scripts/ImmersalServerLocalizer.cs
+++ Assets/ImmersalRESTLocalizerTest/Scripts/ImmersalServerLocalizer.cs
@@ -1,6 +1,12 @@
 ﻿using System;
+using System.Text;
+using ImmersalRESTLocalizer.Types;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Networking;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
+
 
 namespace ImmersalRESTLocalizerTest
 {
@@ -10,6 +16,9 @@ namespace ImmersalRESTLocalizerTest
 
         [SerializeField] private TextMeshPro _textMeshPro;
 
+        [SerializeField] private Texture2D _sampleTexture;
+
+
         private string token;
 
         private void Start()
@@ -17,6 +26,46 @@ namespace ImmersalRESTLocalizerTest
             if (_configuration != null)
             {
                 token = _configuration.token;
+            }
+        }
+
+        public void SendRequest()
+        {
+            SendRequestAsync().ContinueWith(resTask => { Debug.Log(resTask.Result); });
+        }
+
+        private async Task<string> SendRequestAsync()
+        {
+            var base64 = Convert.ToBase64String(_sampleTexture.EncodeToPNG());
+
+            var reqParams = new ImmersalRequestParams
+            {
+                b64 = base64,
+                fx = 1455.738159f,
+                fy = 1455.738159f,
+                ox = 962.615967f,
+                oy = 694.292175f,
+                mapIds = new[] {new MapId {id = 4054}, new MapId {id = 4065}},
+                token = token
+            };
+
+            var request = new UnityWebRequest("https://api.immersal.com/localizeb64", "POST");
+            byte[] byteRaw = Encoding.UTF8.GetBytes(JsonUtility.ToJson(reqParams));
+            request.uploadHandler = new UploadHandlerRaw(byteRaw);
+
+            request.downloadHandler = new DownloadHandlerBuffer();
+            request.SetRequestHeader("Content-Type", "application/json");
+
+            try
+            {
+                var res = await request.SendWebRequest();
+                Debug.Log(res.downloadHandler.text);
+
+                return res.downloadHandler.text;
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
             }
         }
     }
