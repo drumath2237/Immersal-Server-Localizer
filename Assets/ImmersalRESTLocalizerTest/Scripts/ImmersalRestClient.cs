5df489df56bc1c5bfe7dd590e983fc929d1ed653
drumath <2237k.drumath@gmail.com>
2021-09-11 03:00:59 +0900

    :sparkles: リクエスト処理系を分離

---
+++ Assets/ImmersalRESTLocalizerTest/Scripts/ImmersalRestClient.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using Cysharp.Threading.Tasks;
+using ImmersalRESTLocalizer.Types;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.XR.ARSubsystems;
+
+namespace ImmersalRESTLocalizer
+{
+    public class ImmersalRestClient
+    {
+        private readonly ImmersalRESTConfiguration _configuration;
+        private const string URL = "https://api.immersal.com";
+
+        public ImmersalRestClient(ImmersalRESTConfiguration config)
+        {
+            _configuration = config;
+        }
+
+        public async UniTask<string> SendRequestAsync(XRCameraIntrinsics intrinsics, Texture2D cameraTexture)
+        {
+            var base64 = Convert.ToBase64String(cameraTexture.EncodeToPNG());
+
+            var reqParams = new ImmersalRequestParams
+            {
+                b64 = base64,
+                fx = intrinsics.focalLength.x,
+                fy = intrinsics.focalLength.y,
+                ox = intrinsics.principalPoint.x,
+                oy = intrinsics.principalPoint.y,
+                mapIds = _configuration.MapIds,
+                token = _configuration.token
+            };
+
+            var request = new UnityWebRequest($"{URL}/localizeb64", "POST");
+            byte[] byteRaw = Encoding.UTF8.GetBytes(JsonUtility.ToJson(reqParams));
+            request.uploadHandler = new UploadHandlerRaw(byteRaw);
+            request.downloadHandler = new DownloadHandlerBuffer();
+            request.SetRequestHeader("Content-Type", "application/json");
+
+            try
+            {
+                var res = await request.SendWebRequest();
+                return res.downloadHandler.text;
+            }
+            catch (Exception e)
+            {
+                return request.error;
+            }
+        }
+    }
+}
--- Assets/ImmersalRESTLocalizerTest/Scripts/ImmersalServerLocalizer.cs
+++ Assets/ImmersalRESTLocalizerTest/Scripts/ImmersalServerLocalizer.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Text;
-using System.Threading;
-using ImmersalRESTLocalizer.Types;
+﻿using ImmersalRESTLocalizer.Types;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Networking;
 using Cysharp.Threading.Tasks;
-using Unity.Collections;
-using Unity.Collections.LowLevel.Unsafe;
 using Unity.Mathematics;
 using UnityEngine.XR.ARFoundation;
-using UnityEngine.XR.ARSubsystems;
 
 namespace ImmersalRESTLocalizer
 {
@@ -26,19 +19,11 @@ namespace ImmersalRESTLocalizer
 
         [SerializeField] private Transform cameraTransform;
 
-        private Texture2D lastARCameraImage;
-
-        private string _token;
-
-        private MapId[] _mapIds;
-
+        private ImmersalRestClient _immersalRestClient;
+        
         private void Start()
         {
-            if (configuration != null)
-            {
-                _token = configuration.token;
-                _mapIds = configuration.MapIds;
-            }
+            _immersalRestClient = new ImmersalRestClient(configuration);
         }
 
         public void Localize()
@@ -60,11 +45,16 @@ namespace ImmersalRESTLocalizer
             }
 
             var cameraTexture = ARImageProcessingUtil.ConvertARCameraImageToTexture(image);
-
             image.Dispose();
 
-            var resText = await SendRequestAsync(cameraTexture);
+            if (!cameraManager.TryGetIntrinsics(out var intrinsics))
+            {
+                logText.text += "cannot acquire intrinsics\n";
+                return;
+            }
 
+            var resText = await _immersalRestClient.SendRequestAsync(intrinsics, cameraTexture);
+            
             var immersalResponse = JsonUtility.FromJson<ImmersalResponseParams>(resText);
 
             var immersalCameraMatrix = immersalResponse.ToMatrix4();
@@ -74,47 +64,6 @@ namespace ImmersalRESTLocalizer
             arSpace.rotation = mapMatrix.rotation;
         }
         
-        private async UniTask<string> SendRequestAsync(Texture2D texture)
-        {
-            var base64 = Convert.ToBase64String(texture.EncodeToPNG());
-
-            if (!cameraManager.TryGetIntrinsics(out var intrinsics))
-            {
-                logText.text += "cannot get intrinsics\n";
-                return "";
-            }
-
-            var reqParams = new ImmersalRequestParams
-            {
-                b64 = base64,
-                fx = intrinsics.focalLength.x,
-                fy = intrinsics.focalLength.y,
-                ox = intrinsics.principalPoint.x,
-                oy = intrinsics.principalPoint.y,
-                mapIds = _mapIds,
-                token = _token
-            };
-
-            var request = new UnityWebRequest("https://api.immersal.com/localizeb64", "POST");
-            byte[] byteRaw = Encoding.UTF8.GetBytes(JsonUtility.ToJson(reqParams));
-            request.uploadHandler = new UploadHandlerRaw(byteRaw);
-
-            request.downloadHandler = new DownloadHandlerBuffer();
-            request.SetRequestHeader("Content-Type", "application/json");
-
-            try
-            {
-                var res = await request.SendWebRequest();
-                logText.text = res.downloadHandler.text;
-
-                return res.downloadHandler.text;
-            }
-            catch (Exception e)
-            {
-                logText.text = e.ToString();
-                return request.error;
-            }
-        }
 
     }
 }
