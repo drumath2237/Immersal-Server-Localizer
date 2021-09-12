68b96114ef8320de0209c4e24cb783cdcb6ca0a6
drumath <2237k.drumath@gmail.com>
2021-09-12 16:24:24 +0900

    :sparkles: テストを修正

--- Assets/ImmersalRESTLocalizerTest/Tests/ResponseParamsTest.cs
+++ Assets/ImmersalRESTLocalizerTest/Tests/ResponseParamsTest.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using ImmersalRESTLocalizer.Types;
 using UnityEngine;
+using UnityEngine.TestTools.Utils;
 
 namespace Tests
 {
@@ -9,6 +10,7 @@ namespace Tests
         [Test]
         public void ToMatrixTest()
         {
+            var theta = Mathf.PI / 3;
             var res = new ImmersalResponseParams()
             {
                 error = "none",
@@ -17,18 +19,30 @@ namespace Tests
                 px = 1f,
                 py = 1f,
                 pz = 1f,
+                r00 = 1f,
+                r11 = Mathf.Cos(theta),
+                r12 = -Mathf.Sin(theta),
+                r21 = Mathf.Sin(theta),
+                r22 = Mathf.Cos(theta),
             };
 
-            var matrix = new Matrix4x4
-            {
-                m03 = 1f,
-                m13 = 1f,
-                m23 = 1f,
+            var matrix = Matrix4x4.TRS(
+                new Vector3 { x = 1f, y = 1f, z = 1f },
+                Quaternion.AngleAxis(theta * Mathf.Rad2Deg, new Vector3(1, 0, 0)),
+                Vector3.one
+            );
+
+            Assert.That(res.ToMatrix4().GetRow(0),
+                Is.EqualTo(matrix.GetRow(0)).Using(Vector4EqualityComparer.Instance));
                 
-                m33 = 1f
-            };
-            
-            Assert.That(res.ToMatrix4(), Is.EqualTo(matrix));
+            Assert.That(res.ToMatrix4().GetRow(1),
+                Is.EqualTo(matrix.GetRow(1)).Using(Vector4EqualityComparer.Instance));
+                
+            Assert.That(res.ToMatrix4().GetRow(2),
+                Is.EqualTo(matrix.GetRow(2)).Using(Vector4EqualityComparer.Instance));
+                
+            Assert.That(res.ToMatrix4().GetRow(3),
+                Is.EqualTo(matrix.GetRow(3)).Using(Vector4EqualityComparer.Instance));
         }
     }
 }
